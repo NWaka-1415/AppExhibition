0c757e72e961d9c8de1d68fa035feb625d0765f7
Surface Pro 7 <yuuki.ohta.chintap@gmail.com>
2019-12-02 17:36:06 +0900

    アプリケーション終了時にもセーブするようにしました。 細かな表示の調整

--- Assets/C#Scripts/Controllers/OverAllManager.cs
+++ Assets/C#Scripts/Controllers/OverAllManager.cs
@@ -1089,7 +1089,7 @@ namespace Controllers
                 Debug.Log("null");
                 _setGameArg = _isEdit ? _appWindowsManager.GetSelectAppImageFileName() : "";
                 argSelectText.text =
-                    _isEdit ? _appWindowsManager.GetSelectAppImageFileName() : "アプリケーション引数ファイルを選択してください(任意)";
+                    _isEdit ? _appWindowsManager.GetSelectAppArgFileName() : "アプリケーション引数ファイルを選択してください(任意)";
             }
             else
             {
@@ -1098,7 +1098,7 @@ namespace Controllers
                 {
                     _setGameArg = _isEdit ? _appWindowsManager.GetSelectAppImageFileName() : "";
                     argSelectText.text =
-                        _isEdit ? _appWindowsManager.GetSelectAppImageFileName() : "アプリケーション引数ファイルを選択してください(任意)";
+                        _isEdit ? _appWindowsManager.GetSelectAppArgFileName() : "アプリケーション引数ファイルを選択してください(任意)";
                 }
                 else
                 {
@@ -1206,11 +1206,16 @@ namespace Controllers
             _setGameTitle = _appWindowsManager.GetSelectAppTitle();
             _setGameCategory = apps[_appWindowsManager.GetSelectAppNumber()].GameCategory;
             _fileSelectButtonTextOnEdit.GetComponent<Text>().text = _setFileName;
-            _imageFileSelectButtonTextOnEdit.GetComponent<Text>().text = _setImageFileName;
+            _imageFileSelectButtonTextOnEdit.GetComponent<Text>().text =
+                _setImageFileName != "" ? _setImageFileName : "ゲームアイコンのpngファイルを選択してください";
             _titleNameEnterOnEdit.GetComponent<InputField>().text = _setGameTitle;
             //DropDownのやつも元に戻すこと
             _gameCategoryDropDownOnEdit.GetComponent<Dropdown>().value =
                 (int) apps[_appWindowsManager.GetSelectAppNumber()].GameCategory;
+            argFileButtonTextOnEdit.text =
+                _appWindowsManager.GetSelectAppArgFileName() != ""
+                    ? _appWindowsManager.GetSelectAppArgFileName()
+                    : "アプリケーション引数ファイルを選択してください(任意)";
 
             _firstSelectButtonOnEdit.Select();
             _firstSelectButtonOnEdit.OnSelect(null);
@@ -1218,6 +1223,7 @@ namespace Controllers
 
         public void Quit()
         {
+            SaveApplication();
             Application.Quit();
         }
 
